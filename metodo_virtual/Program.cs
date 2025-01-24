5c8b746504936b40d803d32df8915b479cc89f1d
ramonezorrilla <ramoenzorrilla@gmail.com>
2025-01-24 18:30:06 -0400

    Add project files.

---
+++ metodo_virtual/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace metodo_virtual
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Animal miAnimal = new Animal();
+            miAnimal.HacerSonido(); // El animal hace un sonido
+
+            Perro miPerro = new Perro();
+            miPerro.HacerSonido(); // El perro ladra
+
+        }
+
+        public class Animal
+        { 
+            public virtual  void HacerSonido()
+            {
+                Console.WriteLine("El animal hace un sonido");
+            }
+
+        }
+        public class Perro : Animal
+        {
+            public override void HacerSonido()
+            {
+                Console.WriteLine("El perro ladra");
+            }
+        }
+
+
+    }
+
+}
